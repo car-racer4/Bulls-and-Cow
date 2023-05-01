339ff27ddb16e1f30a4e2dd24aa621afb57af06e
SeokjunMoon <seokjun0915@icloud.com>
2023-05-01 20:25:10 +0900

    feat: add bull, cow conuter
    Bull, Cow 개수 카운터 함수 추가

--- Bulls-and-Cow.py
+++ Bulls-and-Cow.py
@@ -1,1 +1,29 @@
-시작
+def get_bull_count(correct_number, input_number):
+    correct_list = list(str(correct_number))
+    input_list = list(str(input_number))
+    counter = 0
+    for i in range(4):
+        if correct_list[i] == input_list[i]:
+            counter += 1
+
+    return counter
+
+
+def number2list(number, list_):
+    number_string = str(number)
+    for i in range(4):
+        list_[int(number_string[i])] += 1
+
+
+def get_cow_count(correct_number, input_number):
+    correct_number_count = [0] * 10
+    input_number_count = [0] * 10
+    number2list(correct_number, correct_number_count)
+    number2list(input_number, input_number_count)
+    counter = 0
+    for i in range(10):
+        temp = min(correct_number_count[i], input_number_count[i])
+        if temp != 0:
+            counter += temp
+
+    return counter - get_bull_count(correct_number, input_number)
